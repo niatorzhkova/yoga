060e8a6f836d169084d083b25e53de5730baa3df
Nia <eugeniamashina@gmail.com>
2024-06-24 14:17:11 +0300

    help

--- index.js
+++ index.js
@@ -1,145 +1,145 @@
-// Скролл до секций
+// // Скролл до секций
 
-function handleClickLink(event) {
-  const block = event.currentTarget.dataset.block;
-  const scrollToBlock = document.getElementById(block);
-  scrollToBlock.style.scrollMargin = "100px";
-  seamless.scrollIntoView(scrollToBlock, {
-    behavior: "smooth",
-    inline: "center",
-    block: "start",
-  });
-}
+// function handleClickLink(event) {
+//   const block = event.currentTarget.dataset.block;
+//   const scrollToBlock = document.getElementById(block);
+//   scrollToBlock.style.scrollMargin = "100px";
+//   seamless.scrollIntoView(scrollToBlock, {
+//     behavior: "smooth",
+//     inline: "center",
+//     block: "start",
+//   });
+// }
 
-document.querySelectorAll(".menu__item").forEach((elem) => {
-  elem.addEventListener("click", handleClickLink);
-});
-// const bookBtn = document.querySelector(".book-btn");
-// bookBtn.addEventListener("click", handleClickLink);
+// document.querySelectorAll(".menu__item").forEach((elem) => {
+//   elem.addEventListener("click", handleClickLink);
+// });
+// // const bookBtn = document.querySelector(".book-btn");
+// // bookBtn.addEventListener("click", handleClickLink);
 
-// Всплывающее меню
+// // Всплывающее меню
 
-function showFixedBanner() {
-  const currentScroll = window.scrollY || document.documentElement.scrollTop;
-  if (currentScroll > 700) {
-    $(".header-fixed").removeClass("hidden");
-    $(".header-fixed").addClass("shown");
-  } else {
-    $(".header-fixed").removeClass("shown");
-    $(".header-fixed").addClass("hidden");
-  }
-}
+// function showFixedBanner() {
+//   const currentScroll = window.scrollY || document.documentElement.scrollTop;
+//   if (currentScroll > 700) {
+//     $(".header-fixed").removeClass("hidden");
+//     $(".header-fixed").addClass("shown");
+//   } else {
+//     $(".header-fixed").removeClass("shown");
+//     $(".header-fixed").addClass("hidden");
+//   }
+// }
 
-function throttle(callee, timeout) {
-  let timer = null;
-  return function perform(...args) {
-    if (timer) return;
-    timer = setTimeout(() => {
-      callee(...args);
-      clearTimeout(timer);
-      timer = null;
-    }, timeout);
-  };
-}
+// function throttle(callee, timeout) {
+//   let timer = null;
+//   return function perform(...args) {
+//     if (timer) return;
+//     timer = setTimeout(() => {
+//       callee(...args);
+//       clearTimeout(timer);
+//       timer = null;
+//     }, timeout);
+//   };
+// }
 
-window.addEventListener("scroll", throttle(showFixedBanner, 250), false);
+// window.addEventListener("scroll", throttle(showFixedBanner, 250), false);
 
-// Скролл клиентов
+// // Скролл клиентов
 
-$.fn.andSelf = function () {
-  return this.addBack.apply(this, arguments);
-};
+// $.fn.andSelf = function () {
+//   return this.addBack.apply(this, arguments);
+// };
 
-const scrollContainer = document.getElementById("clients-scroll");
-const scrollContainerScnd = document.getElementById("clients-scroll-scnd");
-const page = document.querySelector(".page");
+// const scrollContainer = document.getElementById("clients-scroll");
+// const scrollContainerScnd = document.getElementById("clients-scroll-scnd");
+// const page = document.querySelector(".page");
 
-var owl = $(".owl-carousel");
-owl.owlCarousel({
-  loop: true,
-  margin: 0,
-  nav: true,
-  navText: [
-    "<i class='fa fa-caret-left'></i>",
-    "<i class='fa fa-caret-right'></i>",
-  ],
-  touchDrag: true,
-  autoplay: 0.5,
-  autoplayHoverPause: true,
-  responsive: {
-    0: {
-      items: 3,
-    },
-  },
-});
+// var owl = $(".owl-carousel");
+// owl.owlCarousel({
+//   loop: true,
+//   margin: 0,
+//   nav: true,
+//   navText: [
+//     "<i class='fa fa-caret-left'></i>",
+//     "<i class='fa fa-caret-right'></i>",
+//   ],
+//   touchDrag: true,
+//   autoplay: 0.5,
+//   autoplayHoverPause: true,
+//   responsive: {
+//     0: {
+//       items: 3,
+//     },
+//   },
+// });
 
-// Скролл отзывов
+// // Скролл отзывов
 
-const box = document.getElementById("reviews-container");
+// const box = document.getElementById("reviews-container");
 
-let isDown = false;
-let startX;
-let scrollLeft;
+// let isDown = false;
+// let startX;
+// let scrollLeft;
 
-if (box) {
-  box.addEventListener("mousedown", (e) => {
-    isDown = true;
-    startX = e.pageX - box.offsetLeft;
-    scrollLeft = box.scrollLeft;
-    box.style.cursor = "grabbing";
-  });
+// if (box) {
+//   box.addEventListener("mousedown", (e) => {
+//     isDown = true;
+//     startX = e.pageX - box.offsetLeft;
+//     scrollLeft = box.scrollLeft;
+//     box.style.cursor = "grabbing";
+//   });
 
-  box.addEventListener("mouseleave", () => {
-    isDown = false;
-    box.style.cursor = "grab";
-  });
+//   box.addEventListener("mouseleave", () => {
+//     isDown = false;
+//     box.style.cursor = "grab";
+//   });
 
-  box.addEventListener("mouseup", () => {
-    isDown = false;
-    box.style.cursor = "grab";
-  });
+//   box.addEventListener("mouseup", () => {
+//     isDown = false;
+//     box.style.cursor = "grab";
+//   });
 
-  document.addEventListener("mousemove", (e) => {
-    if (!isDown) return;
-    e.preventDefault();
-    const x = e.pageX - box.offsetLeft;
-    const walkX = x - startX;
-    box.scrollLeft = scrollLeft - walkX;
-  });
-}
+//   document.addEventListener("mousemove", (e) => {
+//     if (!isDown) return;
+//     e.preventDefault();
+//     const x = e.pageX - box.offsetLeft;
+//     const walkX = x - startX;
+//     box.scrollLeft = scrollLeft - walkX;
+//   });
+// }
 
-// Скролл "Вы научитесь"
+// // Скролл "Вы научитесь"
 
-const boxLearn = document.getElementById("learn-container");
+// const boxLearn = document.getElementById("learn-container");
 
-let isDownLearn = false;
-let startXLearn;
-let scrollLeftLearn;
+// let isDownLearn = false;
+// let startXLearn;
+// let scrollLeftLearn;
 
-boxLearn.addEventListener("mousedown", (e) => {
-  isDownLearn = true;
-  startXLearn = e.pageX - boxLearn.offsetLeft;
-  scrollLeftLearn = boxLearn.scrollLeft;
-  boxLearn.style.cursor = "grabbing";
-});
+// boxLearn.addEventListener("mousedown", (e) => {
+//   isDownLearn = true;
+//   startXLearn = e.pageX - boxLearn.offsetLeft;
+//   scrollLeftLearn = boxLearn.scrollLeft;
+//   boxLearn.style.cursor = "grabbing";
+// });
 
-boxLearn.addEventListener("mouseleave", () => {
-  isDownLearn = false;
-  boxLearn.style.cursor = "grab";
-});
+// boxLearn.addEventListener("mouseleave", () => {
+//   isDownLearn = false;
+//   boxLearn.style.cursor = "grab";
+// });
 
-boxLearn.addEventListener("mouseup", () => {
-  isDownLearn = false;
-  boxLearn.style.cursor = "grab";
-});
+// boxLearn.addEventListener("mouseup", () => {
+//   isDownLearn = false;
+//   boxLearn.style.cursor = "grab";
+// });
 
-document.addEventListener("mousemove", (e) => {
-  if (!isDownLearn) return;
-  e.preventDefault();
-  const x = e.pageX - boxLearn.offsetLeft;
-  const walkXLearn = x - startXLearn;
-  boxLearn.scrollLeft = scrollLeftLearn - walkXLearn;
-});
+// document.addEventListener("mousemove", (e) => {
+//   if (!isDownLearn) return;
+//   e.preventDefault();
+//   const x = e.pageX - boxLearn.offsetLeft;
+//   const walkXLearn = x - startXLearn;
+//   boxLearn.scrollLeft = scrollLeftLearn - walkXLearn;
+// });
 
 // Youtube player
 
